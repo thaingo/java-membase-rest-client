86aeef4eebeaf195f067e6a9620913c9cbe4d086
Mike Wiederhold <mikewied@comcast.net>
2011-02-28 15:15:10 -0800

    Added lib directory with httpcommons jars and logging jars. Also added function signatures to CouchbaseClient for each rest operation

--- src/com/couchbase/cli/CouchbaseClient.java
+++ src/com/couchbase/cli/CouchbaseClient.java
@@ -1,5 +1,77 @@
 package com.couchbase.cli;
 
 public class CouchbaseClient {
-
+	private String username;
+	private String password;
+	
+	public CouchbaseClient(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String listServers() {
+		
+		return "Server List";
+	}
+	
+	public String serverInfo() {
+		
+		return "Server Info";
+	}
+	
+	public boolean serverAdd() {
+		
+		return true;
+	}
+	
+	public boolean serverReadd() {
+		
+		return true;
+	}
+	
+	public void rebalance() { 
+		
+	}
+	
+	public boolean rebalanceStop() {
+		
+		return true;
+	}
+	
+	public String rebalanceStatus() {
+		
+		return "Status";
+	}
+	
+	public void failover() {
+		
+	}
+	
+	public void clusterInit() {
+		
+	}
+	
+	public void nodeInit() {
+		
+	}
+	
+	public void bucket_list() {
+		
+	}
+	
+	public void bucketCreate() {
+		
+	}
+	
+	public void bucketEdit() {
+		
+	}
+	
+	public String bucketDelete() {
+		return "Not Implemented";
+	}
+	
+	public void bucketFlush() {
+		
+	}
 }
--- src/com/couchbase/cli/CouchbaseResponse.java
+++ src/com/couchbase/cli/CouchbaseResponse.java
@@ -1,5 +1,5 @@
 package com.couchbase.cli;
 
 public class CouchbaseResponse {
-
+	
 }
