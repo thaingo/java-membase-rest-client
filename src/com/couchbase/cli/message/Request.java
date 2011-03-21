91f21e6ef24a12ad8f5102cf0f016bb4c75c648b
Mike Wiederhold <mikewied@comcast.net>
2011-03-21 13:35:27 -0700

    Added documentation for the Request class

--- src/com/couchbase/cli/message/Request.java
+++ src/com/couchbase/cli/message/Request.java
@@ -2,6 +2,14 @@ package com.couchbase.cli.message;
 
 import org.apache.http.client.methods.HttpUriRequest;
 
+/**
+ * Defiens a Request message.
+ */
 public interface Request {
+	
+	/**
+	 * Returns the request created by this object.
+	 * @return An http request that can be sent to the server.
+	 */
 	public HttpUriRequest getRequest();
 }
