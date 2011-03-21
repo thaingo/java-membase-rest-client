0744b5d13ce7ae68c7ee0dba6ffce49fc127b606
Mike Wiederhold <mikewied@comcast.net>
2011-03-21 13:40:40 -0700

    Added documentation to the GetRequest class

--- src/com/couchbase/cli/message/GetRequest.java
+++ src/com/couchbase/cli/message/GetRequest.java
@@ -4,10 +4,20 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 
+/**
+ * Creates a Get message that can be sent to a Couchbase server.
+ */
 public class GetRequest implements Request {
 	private HttpGet request;
 	private String auth;
 	
+	/**
+	 * Creates a get message with base64 authentication.
+	 * @param host The server to send data to.
+	 * @param url The url of REST call.
+	 * @param username The user name for base64 authentication.
+	 * @param password The password for base64 authentication.
+	 */
 	public GetRequest(String host, String url, String username, String password) {
 		request = new HttpGet("http://" + host + ":8091" + url);
 		auth = new String((byte[])((new Base64()).encode((username + ":" + password).getBytes())));
@@ -17,6 +27,7 @@ public class GetRequest implements Request {
 		request.addHeader("Accept", "application/json");
 	}
 	
+	@Override
 	public HttpUriRequest getRequest() {
 		return request;
 	}
