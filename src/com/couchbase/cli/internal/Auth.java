1badd777ee739f46af7dcd703434665caa1b2419
Mike Wiederhold <mikewied@comcast.net>
2011-03-21 13:33:55 -0700

    Added documentation for the Auth class

--- src/com/couchbase/cli/internal/Auth.java
+++ src/com/couchbase/cli/internal/Auth.java
@@ -1,11 +1,26 @@
 package com.couchbase.cli.internal;
 
 public enum Auth {
+	
+	/**
+	 * Specifies no authentication.
+	 */
 	NONE("none"),
+	
+	/**
+	 * Specifies sasl authentication.
+	 */
 	SASL("sasl");
 	
+	/**
+	 * Holds the authentication type.
+	 */
 	public String auth;
 	
+	/**
+	 * Creates an Auth instance.
+	 * @param auth
+	 */
 	Auth(String auth) {
 		this.auth = auth;
 	}
